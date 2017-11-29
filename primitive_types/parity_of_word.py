7002888cd1c9b37cc6cf6b084a9db51dcbbd9ece
sakfnc <sanikaadinath.kulkarni@us.fujitsu.com>
2017-11-28 22:23:09 -0600

    word parity other 2 solutions

--- primitive_types/parity_of_word.py
+++ primitive_types/parity_of_word.py
@@ -9,7 +9,6 @@ def compute_word_parity_one(n):
     """
     Brute force solution iteratively tests the value of each bit while keeping track of the number of 1s seen so far.
     """
-    n = abs(n)
     parity = 0
     while n:
         parity ^= n & 1
@@ -22,7 +21,6 @@ def compute_word_parity_two(n):
     Erases the lowest bit set in a word in a single operation thereby improving performance in the best and average
     cases
     """
-    n = abs(n)
     parity = 0
     while n:
         parity ^= 1
@@ -30,30 +28,60 @@ def compute_word_parity_two(n):
     return parity
 
 
+def compute_word_parity_three(n):
+    """
+    Caching 16 bit parities in dictionary. Complexity is O(n/L) where n is size of word and L is size of cache
+    """
+    PRECOMPILED_PARITY = {}
+    i = 0
+    while i < pow(2, 16):
+        PRECOMPILED_PARITY[i] = compute_word_parity_two(i)
+        i += 1
+    MASK_SIZE = 16
+    BIT_MASK = 0xFFFF
+    parity = PRECOMPILED_PARITY[n >> (3 * MASK_SIZE)] ^ PRECOMPILED_PARITY[(n >> (2 * MASK_SIZE)) & BIT_MASK] ^ \
+             PRECOMPILED_PARITY[(n >> MASK_SIZE) & BIT_MASK] ^ PRECOMPILED_PARITY[n & BIT_MASK]
+    return parity
+
+
+def compute_word_parity_four(n):
+    """
+    This method XORs the last half of the word with the first half and returns the final bit.
+    """
+    n ^= n >> 32
+    n ^= n >> 16
+    n ^= n >> 8
+    n ^= n >> 4
+    n ^= n >> 2
+    n ^= n >> 1
+    return n & 0x1
+
+
 class ParityOfWordTest(unittest.TestCase):
     def test_one(self):
         self.assertEqual(compute_word_parity_one(1234), 1)
         self.assertEqual(compute_word_parity_two(1234), 1)
+        self.assertEqual(compute_word_parity_three(1234), 1)
+        self.assertEqual(compute_word_parity_four(1234), 1)
 
     def test_two(self):
         self.assertEqual(compute_word_parity_one(123), 0)
         self.assertEqual(compute_word_parity_two(123), 0)
+        self.assertEqual(compute_word_parity_three(123), 0)
+        self.assertEqual(compute_word_parity_four(123), 0)
 
     def test_three(self):
         self.assertEqual(compute_word_parity_one(0), 0)
         self.assertEqual(compute_word_parity_two(0), 0)
+        self.assertEqual(compute_word_parity_three(0), 0)
+        self.assertEqual(compute_word_parity_four(0), 0)
 
     def test_four(self):
         self.assertEqual(compute_word_parity_one(9223372036854775807), 1)
         self.assertEqual(compute_word_parity_two(9223372036854775807), 1)
+        self.assertEqual(compute_word_parity_three(9223372036854775807), 1)
+        self.assertEqual(compute_word_parity_four(9223372036854775807), 1)
 
-    def test_five(self):
-        self.assertEqual(compute_word_parity_one(-123), 0)
-        self.assertEqual(compute_word_parity_two(-123), 0)
-
-    def test_six(self):
-        self.assertEqual(compute_word_parity_one(-4), 1)
-        self.assertEqual(compute_word_parity_two(-4), 1)
 
 if __name__ == "__main__":
     unittest.main()
